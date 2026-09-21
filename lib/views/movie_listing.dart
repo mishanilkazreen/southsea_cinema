d4d3e4bfb61d66a492617390d7cceb5d8633b331
mishanil <mishanilkazreen@gmail.com>
2026-09-21 21:44:59 +0100

    feat: added film title & description text widgets

--- lib/views/movie_listing.dart
+++ lib/views/movie_listing.dart
@@ -15,7 +15,16 @@ class MovieListing extends StatelessWidget {
         elevation: 0,
       ),
       drawer: const NavDrawer(),
-      body: const SizedBox.shrink(),
+      body: Container(
+        child: Column(
+          children: [
+            Text('Interstellar'),
+            const SizedBox(height: 16.0),
+            Text(
+              'When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.')
+          ],
+        )
+      )
     );
   }
 }
