cae11fee321dc3cd3fb9d927dbd8f31a6a288e3d
mishanil <mishanilkazreen@gmail.com>
2026-09-21 18:40:03 +0100

    feat: set up the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,3 @@
 import 'package:flutter/material.dart';
-import 'package:southsea_cinema/constants.dart';
-import 'package:southsea_cinema/views/home_view.dart';
-import 'package:southsea_cinema/views/movie_listing.dart';
 
-void main() {
-  runApp(const SouthseaCinemaApp());
-}
-
-class SouthseaCinemaApp extends StatelessWidget {
-  const SouthseaCinemaApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: appTitle,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: cinemaBackground,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: cinemaBrand,
-          primary: cinemaBrand,
-          surface: cinemaSurface,
-        ),
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const HomeView(),
-        '/listing': (context) => const MovieListing(),
-      },
-    );
-  }
-}
+void main() {}
