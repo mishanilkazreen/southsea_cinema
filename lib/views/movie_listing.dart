7c43e5b11a38ae19295fb2201013cf4c22ea0062
mishanil <mishanilkazreen@gmail.com>
2026-09-22 23:28:41 +0100

    style: document format

--- lib/views/movie_listing.dart
+++ lib/views/movie_listing.dart
@@ -16,119 +16,115 @@ class _MovieListingState extends State<MovieListing> {
 
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-      appBar: AppBar(
-        title: const Text(appTitle, style: cinemaHeaderStyle),
-        backgroundColor: cinemaSurface, 
-        iconTheme: const IconThemeData(color: cinemaBrand),
-        elevation: 0,
-      ),
-      drawer: const NavDrawer(),
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          spacing: 16.0,
-          children: [
-            const Text(
-              'Interstellar (2014) (PG-13)',
-              style: TextStyle(
-                color: cinemaFontWhite,
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const Text(
-              'When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.',
-              style: TextStyle(color: cinemaFontWhite),
-              ),
-            const Text(
-              'Southsea Cinema Room\nWednesday 23rd Sep 2026, 18:00 - ends at 19:54',
-              style: TextStyle(color: cinemaFontWhite),
-            ),
-            const Text(
-              'Please note that Discounts / Membership Benefits will be applied once you have selected your tickets\nSelect Quantities (Up to 5 in total)',
-              style: TextStyle(color: cinemaFontWhite),
-            ),
-            const Text(
-              'Tickets',
-              style: TextStyle(
-                color: cinemaFontWhite,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            LayoutBuilder(
-              builder: (context, constraints) {
-                if (constraints.maxWidth > 250) {
-                  return Row(
-                    spacing: 16.0,
-                    children: [
-                      DropdownMenu<int>(
-                        initialSelection: 0,
-                        onSelected: (int? value) {
-                          if (value != null) {
-                            setState(() {
-                              _quantity = value;
-                            });
-                          }
-                        },
-                        dropdownMenuEntries: [
-                          for (int i = 0; i <= 5; i++)
-                            DropdownMenuEntry(value: i, label: '$i')
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle, style: cinemaHeaderStyle),
+          backgroundColor: cinemaSurface,
+          iconTheme: const IconThemeData(color: cinemaBrand),
+          elevation: 0,
+        ),
+        drawer: const NavDrawer(),
+        body: Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 16.0,
+              children: [
+                const Text(
+                  'Interstellar (2014) (PG-13)',
+                  style: TextStyle(
+                    color: cinemaFontWhite,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Text(
+                  'When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.',
+                  style: TextStyle(color: cinemaFontWhite),
+                ),
+                const Text(
+                  'Southsea Cinema Room\nWednesday 23rd Sep 2026, 18:00 - ends at 19:54',
+                  style: TextStyle(color: cinemaFontWhite),
+                ),
+                const Text(
+                  'Please note that Discounts / Membership Benefits will be applied once you have selected your tickets\nSelect Quantities (Up to 5 in total)',
+                  style: TextStyle(color: cinemaFontWhite),
+                ),
+                const Text(
+                  'Tickets',
+                  style: TextStyle(
+                    color: cinemaFontWhite,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                LayoutBuilder(
+                  builder: (context, constraints) {
+                    if (constraints.maxWidth > 250) {
+                      return Row(
+                        spacing: 16.0,
+                        children: [
+                          DropdownMenu<int>(
+                            initialSelection: 0,
+                            onSelected: (int? value) {
+                              if (value != null) {
+                                setState(() {
+                                  _quantity = value;
+                                });
+                              }
+                            },
+                            dropdownMenuEntries: [
+                              for (int i = 0; i <= 5; i++)
+                                DropdownMenuEntry(value: i, label: '$i')
+                            ],
+                          ),
+                          const Text(
+                            "Adults (£7.50)",
+                            style: TextStyle(color: cinemaFontWhite),
+                          )
                         ],
-                      ),
-                      const Text(
-                        "Adults (£7.50)",
-                        style: TextStyle(color: cinemaFontWhite),
-                      )
-                    ],
-                  );
-                } else {
-                  return Column(
-                    spacing: 16.0,
-                    children: [
-                      DropdownMenu<int>(
-                        initialSelection: 0,
-                        onSelected: (int? value) {
-                          if (value != null) {
-                            setState(() {
-                              _quantity = value;
-                            });
-                          }
-                        },
-                        dropdownMenuEntries: [
-                          for (int i = 0; i <= 5; i++)
-                            DropdownMenuEntry(value: i, label: '$i')
+                      );
+                    } else {
+                      return Column(
+                        spacing: 16.0,
+                        children: [
+                          DropdownMenu<int>(
+                            initialSelection: 0,
+                            onSelected: (int? value) {
+                              if (value != null) {
+                                setState(() {
+                                  _quantity = value;
+                                });
+                              }
+                            },
+                            dropdownMenuEntries: [
+                              for (int i = 0; i <= 5; i++)
+                                DropdownMenuEntry(value: i, label: '$i')
+                            ],
+                          ),
+                          const Text(
+                            "Adults (£7.50)",
+                            style: TextStyle(color: cinemaFontWhite),
+                          )
                         ],
-                      ),
-                      const Text(
-                        "Adults (£7.50)",
-                        style: TextStyle(color: cinemaFontWhite),
-                      )
-                    ],
-                  );
-                }
-              },
-            ),
-            ElevatedButton(
-              // TO-DO: Convert to a Popup Notification
-              style: ElevatedButton.styleFrom(
-                backgroundColor: cinemaBrandDark,
-                foregroundColor: cinemaFontWhite,
-              ),
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('$_quantity adult tickets added to order')
-                    )
-                  );
-              },
-              child: const Text('ADD TO ORDER')
-            ), // TO-DO: Convert to a Popup Notification
-          ],
-        )
-      )
-    );
+                      );
+                    }
+                  },
+                ),
+                ElevatedButton(
+                    // TO-DO: Convert to a Popup Notification
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: cinemaBrandDark,
+                      foregroundColor: cinemaFontWhite,
+                    ),
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content:
+                              Text('$_quantity adult tickets added to order')));
+                    },
+                    child: const Text(
+                        'ADD TO ORDER')), // TO-DO: Convert to a Popup Notification
+              ],
+            )));
   }
 }
